9b5fe7648b0b4a211a758db88869887b9df8df98
ontwerpduurzaam <info@ontwerpduurzaam.nl>
2013-12-05 23:11:24 +0100

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,12 @@
 ActiveRecord::Schema.define(version: 20131205192507) do
 
   create_table "users", force: true do |t|
+    t.string   "remember_token"
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
